d534b5750b3c705ad99bfcd69c86003561487227
Andy <349562769@gapps.yrdsb.ca>
2023-08-03 00:18:47 -0400

    plleeeeeeeesssssse work Im beggging klaflajflakfj

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ function App() {
     height:30px;
   `
   //const lottoData = [{date:"Bruh",Number:[1,2,3,4,5,6,7], bonusNum: 8},{date:"Bruh2",Number:[0,1,2,3,4,5,6],bonusNum:7}, {date:"Bruh3",Number:[0,0,0,0,0,0,0],bonusNum:5}];
-  const {data,error,loading} = useFetch("http://coconut-awake-helicona.glitch.me/numbers/")
+  const {data,error,loading} = useFetch("https://coconut-awake-helicona.glitch.me/numbers/")
   const lottoData = data;
 
   const [userNumbers, setUserNumbers] = useState([]);
